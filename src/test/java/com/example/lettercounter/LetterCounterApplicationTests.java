f4ee014b6e89331deb0af4b477fd10723e0d430c
christina <darthchristina8@gmail.com>
2023-10-05 16:29:30 +0300

    class LetterCounterController created

---
+++ src/test/java/com/example/lettercounter/LetterCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lettercounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
